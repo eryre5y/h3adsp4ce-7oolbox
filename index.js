dc0c8f3c46b7570a5a90a152f06245315628800a
basil <mikus1337112@gmail.com>
2022-06-05 00:12:50 +0300

    heroku test

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ const client = new Client({
     ],
 });
 
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(80);
+
 const Discord = require('discord.js');
 
 // Call .env file to get Token
