359018138df9b72e416dfdddeb1b5d016248fc6e
basil <mikus1337112@gmail.com>
2022-06-04 15:03:29 +0300

    embed experiment

--- slashCommands/Utility/me.js
+++ slashCommands/Utility/me.js
@@ -1,3 +1,5 @@
+const { MessageSelectMenu } = require("discord.js");
+
 module.exports = {
     name: "me",
     usage: "/me <text>",
@@ -15,13 +17,12 @@ module.exports = {
     run: async (client, interaction) => {
         // await interaction.channel.sendTyping();
 
-        const text = interaction.options.getString("text");
-        
         const meEmbed = new client.discord.MessageEmbed()
         .setDescription(`**<@${interaction.member.id}>** ${text}`)
         .setColor(client.config.embedColor)
         .setFooter({ text: `${client.config.embedfooterText}`, iconURL: `${client.user.displayAvatarURL()}` });
 
         await interaction.reply({ embeds: [meEmbed] });
+        
     },
 };
---
+++ slashCommands/Utility/quote.js
@@ -0,0 +1,32 @@
+module.exports = {
+    name: "quote",
+    options: [
+        {
+            name: 'text',
+            description: 'your quote',
+            type: 'STRING',
+            required: true
+        }
+    ],
+    category: "Utility",
+    description: "create your quote",
+    ownerOnly: false,
+    run: async (client, interaction) => {
+        // await interaction.channel.sendTyping();
+
+        const msg = await interaction.channel.send(`:scroll:Creating quote`);
+
+        const text = interaction.options.getString("text");
+
+        const textEmbed = new client.discord.MessageEmbed()
+            .setDescription(`_${text}_`)
+            .setColor(client.config.embedColor)
+            .addField("Author", `**<@${interaction.member.id}>**`, true)
+            .setFooter({ text: `${client.config.embedfooterText}`, iconURL: `${client.user.displayAvatarURL()}` });
+
+        await interaction.reply({embeds: [textEmbed] });
+
+        msg.delete();
+    },
+};
+    
